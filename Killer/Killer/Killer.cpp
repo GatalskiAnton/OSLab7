d8c8ed240a7f2379b43223299d3eeaa1873cf19e
AntonGatalski <ag629331@gmail.com>
2022-11-27 16:32:31 +0300

    zabil sdelat commit

---
+++ Killer/Killer/Killer.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3322d2a7f45e2aa29e5133db0c566b7d9b341b96de5a5164b78517e4430550f4
+size 2056
---
+++ User/User/User.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ca677a940a28732fa1322222cbf147ced8ce3297d8bc1d31bd488f4d11434579
+size 1378
